5ec6f08cca701a1085df09c86ed54e8fa6a99577
azptmdr <ksgarciap@unbosque.edu.co>
2023-05-28 16:36:44 -0500

    Base de Datos

--- baseDeDatos/apps.py
+++ baseDeDatos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GestionusuarioConfig(AppConfig):
+class BasededatosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestionUsuario'
+    name = 'baseDeDatos'
