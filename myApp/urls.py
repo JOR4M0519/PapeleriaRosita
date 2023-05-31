fb305e3edaac42dd9b500cd9d2b4fbcfdbc1bca6
yopalito <jdramoss@unbosque.edu.co>
2023-05-30 23:45:07 -0500

    Version (env+Rest)

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,6 +1,9 @@
+from rest_framework import routers
 from django.urls import path
 from . import views
 
+router = routers.DeultRouter()
+
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
 ]
