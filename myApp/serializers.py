0a46f3e3bbb23e760f5e9a8991260827ee5aa9bf
yopalito <jdramoss@unbosque.edu.co>
2023-06-02 10:23:55 -0500

    Model implementation

--- myApp/serializers.py
+++ myApp/serializers.py
@@ -5,17 +5,13 @@ from .models import Usuario,Proveedor,Producto,DetallesVenta,DetallesCompra
 
 #view set -> djanog convierte datos python en json & quien puede acceder a los datos
 
-class UsuarioSerializer(serializers.UsuarioSerializer):
+class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
-        managed = True
-        db_table = 'usuario'
         model = Usuario
         fields = ('id_usuario','username','nombre','telefono','num_identificacion','clave','rol','estado')
         #read_only_fields = ('cread_at')
 
-class ProductoSerializer(serializers.ProductoSerializer):
+class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
-        managed = True
-        db_table = 'producto'
         model = Producto
         fields = ('id_producto','nombre_producto','valor_compra','valor_venta','valor_ganancia','stock','estado')
--- myApp/urls.py
+++ myApp/urls.py
@@ -7,9 +7,10 @@ router = routers.DefaultRouter()
 router.register('api/productos',ProductoViewSet,'producto')
 router.register('api/usuarios',UsuarioViewSet,'usuario')
 
-urlpatterns = router.urls,[
+urlpatterns = [
     # path('', views.index),
-
     path('producto/', views.ProductoView.as_view()),
     path('producto/<int:id>', views.ProductoView.as_view())
 ]
+
+urlpatterns += router.urls
