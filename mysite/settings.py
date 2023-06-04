d03a0de08c7d18fa04eb2c993c1106c1b842549a
Lau-lalo11 <lamateus@unbosque.edu.co>
2023-06-04 13:00:32 -0500

    filtro

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'PapeleriaRosita',
+        'NAME': 'papeleriarosita',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
