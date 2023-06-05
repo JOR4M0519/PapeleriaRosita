9648a890eb572e1ba6929175b4bf31ebef52143f
Azptmdr <ksgarciap@unbosque.edu.co>
2023-06-04 19:55:18 -0500

    Trigger Cambios de Producto

--- myApp/sql/Triggers.sql
+++ myApp/sql/Triggers.sql
@@ -53,10 +53,53 @@ ON producto
     FOR EACH ROW
     WHEN(pg_trigger_depth()<1)
     EXECUTE FUNCTION FN_Ganancia();
+-- ----------------------- TRIGGER CAMBIOS --------------------------------
+CREATE OR REPLACE FUNCTION FN_Cambios()
+RETURNS TRIGGER
+AS $$
+BEGIN
+    IF new.id_producto <> old.id_producto THEN
+        UPDATE producto
+        SET stock=producto.stock+old.cantidad
+        WHERE id_producto = old.id_producto;
 
-UPDATE producto
-SET valor_venta = 1700
-WHERE id_producto = 10;
+        UPDATE producto
+        SET stock = producto.stock-new.cantidad
+        WHERE id_producto = new.id_producto;
 
-SELECT * FROM producto WHERE id_producto = 10;
+    ELSIF new.id_producto = old.id_producto AND old.cantidad > new.cantidad THEN
 
+        UPDATE producto
+        SET stock = producto.stock+(old.cantidad-new.cantidad)
+        WHERE id_producto = new.cantidad;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER TR_Cambios
+AFTER UPDATE
+ON detalles_venta
+    FOR EACH ROW
+    WHEN(pg_trigger_depth()<1)
+    EXECUTE FUNCTION FN_Cambios();
+
+
+UPDATE detalles_venta
+SET id_producto = 2,cantidad = 1
+WHERE id_detventa = 6;
+
+INSERT INTO producto (nombre_producto, valor_compra, valor_venta, valor_ganancia, stock, estado)
+VALUES ('Calculadora',5000,8000,0,10,'A');
+
+
+
+SELECT  id_detventa,id_producto,cantidad,
+       (select nombre_producto from producto where detalles_venta.id_producto = producto.id_producto),
+       (select stock from producto where detalles_venta.id_producto = producto.id_producto)
+FROM detalles_venta;
+
+SELECT * FROM producto;
+
+SELECT nombre_producto,stock from producto where stock < 10;
